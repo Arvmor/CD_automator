07c440e6ca552a144927ac65ab7e126d8fcf43e6
Arvin Morawej <arvin.morawej@gmail.com>
2020-05-20 20:23:49 +0430

    Adding delays
    with time library you can add delays in your code,
    if it didn't work (which i dubt), you have to delay the "typing" action
    it means setting delay for each chrachter in your username&password while typing.

--- automator.py
+++ automator.py
@@ -1,24 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
-
-PATH = r"C:\Users\your_name\Downloads\chromedriver_win32\chromedriver.exe" #or wherever you save the chrome driver to
+# or wherever you save the chrome driver to
+PATH = r"C:\Users\your_name\Downloads\chromedriver_win32\chromedriver.exe"
 
 
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://verified.capitalone.com/auth/signin?Product=Card")
 
-name = driver.find_element_by_id('ods-input-0')
-
-name.send_keys("username") #this would be your capital one username
-
-password = driver.find_element_by_id('ods-input-1')
-
-password.send_keys('password') #capital one password
-
-
-submit = driver.find_element_by_xpath('/html/body/app-root/div/div/app-sign-in/ci-content-card/div/div/ngx-ent-signin/form/p[2]/button')
-submit.submit()
-
-
+# this would be your capital one username
+driver.find_element_by_id('ods-input-0').send_keys("username")
+time.sleep(2)
+driver.find_element_by_id(
+    'ods-input-1').send_keys('password')  # capital one password
+time.sleep(2)
+driver.find_element_by_xpath(
+    '/html/body/app-root/div/div/app-sign-in/ci-content-card/div/div/ngx-ent-signin/form/p[2]/button').click()
